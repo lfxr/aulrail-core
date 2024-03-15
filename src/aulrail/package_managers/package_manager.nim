da0ef9c22b35c4ebd0f120c59b520852ebb58432
lafixier <100778884+lafixier@users.noreply.github.com>
2024-03-15 22:46:35 +0900

    feat(PackageManager): add PackageManager

---
+++ src/aulrail/package_managers/package_manager.nim
@@ -0,0 +1,30 @@
+import
+  options,
+  os,
+  osproc
+
+import
+  ../result,
+  ../types
+
+
+func newPackageManager*(appPath: string): ref PackageManager =
+  result = new PackageManager
+  result.appPath = appPath
+
+
+proc launch*(pm: ref PackageManager): Result[void] =
+  ## PackageManagerを起動
+  if not pm.appPath.fileExists:
+    result.error = option(Error(
+      kind: ErrorKind.fileDoesNotExists,
+      path: pm.appPath
+    ))
+    return
+  try:
+    discard execProcess(pm.appPath, options={})
+  except:
+    result.error = option(Error(
+      kind: ErrorKind.failedToLaunchPackageManager,
+      executedCommand: pm.appPath
+    ))
